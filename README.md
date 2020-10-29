ecb0603ba7dad798e009586626a4a7657e9ef940
Erik <erik@bitstorm.se>
2020-10-29 21:48:54 +0100

    Added the possibility to create and use a config file with tags to process, and the ability to list available OPC DA servers on the specified source

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This very basic program collects all tags from an OPC DA source once a second and sends them in JSON format over UDP to a single-cast receiver IP.
 
 The primary use of this program is for replicating real time data from sensitive systems through a data diode to a potentially hostile network in order to achieve complete isolation from the external network.
+
+On the outer side of the data diode, use [dd-inserter](https://github.com/cyops-se/dd-inserter) to store the data in a Timescale database. More one way data receivers will be added in the future.
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net"
 	"time"
 
@@ -22,12 +24,32 @@ type DataMessage struct {
 	Points []DataPoint `json:"points"`
 }
 
+type ConfigTagEntry struct {
+	Name string `json:"name"`
+}
+
+type Config struct {
+	Tags []ConfigTagEntry `json:"tags"`
+}
+
 func main() {
 	target := flag.String("target", "172.16.11.3", "The address of the outer inserter")
 	port := flag.Int("port", 4357, "The UDP port of the outer inserter")
-	source := flag.String("source", "192.168.0.206", "The address of the OPC server")
+	source := flag.String("source", "localhost", "The address of the OPC server")
 	progID := flag.String("progid", "IntegrationObjects.AdvancedSimulator.1", "The OPC server prog id")
+	config := flag.String("c", "", "Configuration file, for example declaring tags to process. If not specified, all tags will be processed")
+	create := flag.Bool("create", false, "Use this parameter to create a config file with all tags found in the specified server")
+	list := flag.Bool("list", false, "Lists the OPC DA servers available on the specified source")
 	flag.Parse()
+
+	if *list {
+		servers_found := opc.NewAutomationObject().GetOPCServers(*source)
+		log.Printf("Found %d server(s) on '%s':\n", len(servers_found), *source)
+		for _, server := range servers_found {
+			log.Println(server)
+		}
+		return
+	}
 
 	con, err := net.Dial("udp", fmt.Sprintf("%s:%d", *target, *port))
 	defer con.Close()
@@ -40,11 +62,47 @@ func main() {
 		return
 	}
 
+	fmt.Println("Available tags")
 	opc.PrettyPrint(browser)
+	tags := opc.CollectTags(browser)
+
+	// Check config and use tags defined there if any
+	if *config != "" {
+		if !*create {
+			if data, err := ioutil.ReadFile(*config); err == nil {
+				c := &Config{}
+				if err = json.Unmarshal(data, &c); err == nil && len(c.Tags) > 0 {
+					tags := make([]string, len(c.Tags))
+					for i, tag := range c.Tags {
+						tags[i] = tag.Name
+					}
+				} else {
+					log.Printf("Unable to interpret config file, %s. Will be using all tags found in the server. Error: %v\n", *config, err)
+				}
+			} else {
+				log.Printf("Unable to open specified config file, %s. Will be using all tags found in the server. Error: %v\n", *config, err)
+			}
+		} else {
+			c := &Config{}
+			c.Tags = make([]ConfigTagEntry, len(tags))
+			for i, tag := range tags {
+				c.Tags[i].Name = tag
+			}
+			if data, err := json.MarshalIndent(c, "", "    "); err == nil {
+				if err = ioutil.WriteFile(*config, data, 0644); err != nil {
+					log.Printf("Unable to write JSON config file, %s. Error: %v\n", *config, err)
+				} else {
+					return
+				}
+			} else {
+				log.Printf("Unable to create JSON for config file, %s. Error: %v\n", *config, err)
+			}
+		}
+	}
 
 	client, err := opc.NewConnection(*progID, // ProgId
-		[]string{*source},        //  OPC servers nodes
-		opc.CollectTags(browser), // slice of OPC tags
+		[]string{*source}, //  OPC servers nodes
+		tags,              // slice of OPC tags
 	)
 	defer client.Close()
 
