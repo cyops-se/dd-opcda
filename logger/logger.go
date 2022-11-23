238057cc5c077e7e0900ff81b0e8b864a69fee39
Erik <erik@bitstorm.se>
2022-11-23 11:36:21 +0100

    added purge of logs

--- logger/logger.go
+++ logger/logger.go
@@ -14,10 +14,9 @@ func Log(category string, title string, msg string) string {
 	if db.DB != nil {
 		db.DB.Create(&entry)
 	}
-	if category == "error" {
-	}
 	text := fmt.Sprintf("%s: %s, %s", category, title, msg)
 	log.Printf(text)
+	purge()
 	return text
 }
 
@@ -32,4 +31,17 @@ func Error(title string, format string, args ...interface{}) error {
 	text := Log("error", title, msg)
 	NotifySubscribers("logger.error", fmt.Sprintf("%s: %s", title, msg))
 	return fmt.Errorf(text)
+}
+
+func purge() {
+	var result int64
+	if db.DB != nil {
+		db.DB.Model(&types.Log{}).Count(&result)
+		for result > 1000 {
+			var first types.Log
+			db.DB.First(&first)
+			db.DB.Unscoped().Delete(&first)
+			result--
+		}
+	}
 }
